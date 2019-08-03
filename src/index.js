78b2a43defa0b05d6bc090958faa70829a38e166
André Murilo <lifercode@gmail.com>
2019-08-02 23:58:52 -0300

    Set initial structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Routes from './routes'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Routes />, document.getElementById('root'))
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Home from './pages/Home'
+
+export default function Routes() {
+  return (
+    <>
+      <Home />
+    </>
+  )
+}
