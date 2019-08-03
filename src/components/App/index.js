4122c2bbbabb36570decbd21bac1188914ddbc0b
André Murilo <lifercode@gmail.com>
2019-08-03 00:18:06 -0300

    Set global styles

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Routes from '../../routes'
+import GlobalStyles from './styles'
+
+export default function App() {
+  return (
+    <>
+      <Routes />
+      <GlobalStyles />
+    </>
+  )
+}
