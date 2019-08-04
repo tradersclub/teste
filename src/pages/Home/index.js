f081f400af9dcd605eb672b975baba0995c63fa2
André Murilo <lifercode@gmail.com>
2019-08-04 12:58:09 -0300

    Display cars in home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Page from '../../components/Page'
+import CarsList from '../../components/CarsList'
 import { Wellcome } from './styles'
 
-export default function Home() {
-  return (
-    <Page navbar>
+export default ({ cars }) =>
+  <Page navbar>
+    {cars && cars.length ?
+      <CarsList cars={cars} />
+    :
       <Wellcome>
         Pesquisa de veículos do <span>TradersClub</span>
       </Wellcome>
-    </Page>
-  )
-}
+    }
+  </Page>
