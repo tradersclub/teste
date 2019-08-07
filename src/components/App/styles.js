54c33d83d7adba2f31dfa1ddfd95804b12bbdade
André Murilo <lifercode@gmail.com>
2019-08-06 21:07:47 -0300

    Update global styles

--- src/components/App/styles.js
+++ src/components/App/styles.js
@@ -2,6 +2,12 @@ import React from 'react'
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
   html, body, #root {
     height: 100%;
   }
@@ -12,6 +18,10 @@ const GlobalStyles = createGlobalStyle`
     background-color:#1f2e40;
     color:#f5f5f5;
     font-family: 'Roboto', sans-serif;
+    -webkit-font-smoothing: antialiased !important;
+  }
+  ul {
+    list-style: none;
   }
   .app-container {
     display: flex;
