8a32733a4ad4b20d7add90ecb300bd03d37c3609
André Murilo <lifercode@gmail.com>
2019-08-07 10:34:07 -0300

    Remove comments in code

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import { Container } from './styles'
-// import Sidebar from '../Sidebar'
 import Routes from '../../routes'
 
 export default () =>
   <Container>
-    {/* <Sidebar /> */}
     <Routes />
   </Container>
