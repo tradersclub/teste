95aa0ad156ef021355eb9282322109efb39efdee
André Murilo <lifercode@gmail.com>
2019-08-07 10:25:54 -0300

    Import roboto/lobster fonts

--- src/components/App/styles.js
+++ src/components/App/styles.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Lobster+Two:400,700|Roboto:400,700&display=swap');
   * {
     margin: 0;
     padding: 0;
