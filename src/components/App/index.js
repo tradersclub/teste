602214131c876683772123d4edbf41bda80f3aab
Lucas Barga <lucasmpbarga@gmail.com>
2019-10-31 22:13:38 -0300

    implementando menu lateral flex

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,21 @@
-import React, {Component} from 'react';
-
+import React, { Component } from 'react';
 import '../../assets/styles/Shared.css';
+import logo from '../../assets/img/logo-tc.png';
 
 export default class App extends Component {
+
     render() {
         return (
-            <h1>Teste do Tradersclub</h1>
+            <div style={{
+                display: 'flex', height: '100vh'
+            }}>
+                <section class='menu'>
+                    <div class='logo-container'>
+                        <img src={logo} alt="Logo" />
+                    </div>
+                </section>
+                <div style={{ flex: 8, backgroundColor: 'yellow' }}></div>
+            </div>
         );
     }
 }
